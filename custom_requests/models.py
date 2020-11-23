b99c6040700f589bc92e879eea6c23fd4b6fb082
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-23 16:11:50 +0300

    Creates a user request model
    Also adds migrations to create it

--- custom_requests/models.py
+++ custom_requests/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+REQUESTS_STATUS_TEXT = 'Select requests status'
+REQUESTS_STATUS = [
+    ('drf', 'draft'),
+    ('snt', 'sent'),
+    ('acc', 'accepted'),
+    ('rej', 'rejected')
+]
+
+
+class Requests(models.Model):
+    text = models.CharField(max_length=100, help_text="Enter the text of your application")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=REQUESTS_STATUS, help_text=REQUESTS_STATUS_TEXT)
+
+    def __str__(self):
+        return '{0} /{1} /{2}'.format(self.user, self.creation_date, self.status)
+
+    class Meta:
+        verbose_name = 'User request'
+        verbose_name_plural = 'User requests'
