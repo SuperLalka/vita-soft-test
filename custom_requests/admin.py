9e8c16916a11d28d14c4f4e26b71dd78af632a69
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-23 16:16:25 +0300

    Adds a request model to the admin panel

--- custom_requests/admin.py
+++ custom_requests/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.db import models as django_models
+from django.forms import TextInput
 
-# Register your models here.
+
+from . import models
+
+
+@admin.register(models.Requests)
+class CategoriesAdmin(admin.ModelAdmin):
+    date_hierarchy = 'creation_date'
+    formfield_overrides = {
+        django_models.CharField: {'widget': TextInput(attrs={'size': '100'})}
+    }
+    list_display = ('creation_date', 'user', 'status')
+    list_filter = ('user', 'status')
