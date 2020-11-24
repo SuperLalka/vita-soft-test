2a16e4c205f766618222eed201b336bb347a1f7c
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-24 22:47:04 +0300

    Adds url patterns for websets & to work with JWT tokens

--- custom_requests/urls.py
+++ custom_requests/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url
+from django.urls import include
 
+from rest_framework.routers import DefaultRouter
 from . import views
+
+
+routerAPI = DefaultRouter()
+routerAPI.register(r'requests', views.RequestsViewSet, basename='requests')
+routerAPI.register(r'users', views.UsersViewSet, basename='users')
 
 
 app_name = 'custom_requests'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^api/', include(routerAPI.urls)),
 ]
--- vita_soft/urls.py
+++ vita_soft/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
+
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
+
+
+tokens_url = [
+    path('', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('verify/', TokenVerifyView.as_view()),
+]
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', include(tokens_url)),
     path('', include('custom_requests.urls')),
 ]
+
