161b6ae5ef98cd420e937f83485ed2f6ae549022
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-25 23:19:06 +0300

    Changes the models in the project DB to use m2m connections

--- custom_requests/models.py
+++ custom_requests/models.py
@@ -2,42 +2,63 @@ from django.conf import settings as django_settings
 from django.contrib.auth.models import User
 from django.db import models
 
+from vita_soft import constants
 
 REQUESTS_STATUS_TEXT = 'Select requests status'
-REQUESTS_STATUS = [
-    ('drf', 'draft'),
-    ('snt', 'sent'),
-    ('acc', 'accepted'),
-    ('rej', 'rejected')
-]
+USER_ROLES_TEXT = 'Select user role'
 
 
 class Requests(models.Model):
     text = models.CharField(max_length=100, help_text="Enter the text of your application")
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    creation_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=REQUESTS_STATUS, help_text=REQUESTS_STATUS_TEXT,
-                              editable=False, default='drf')
+    status = models.CharField(max_length=20, choices=constants.REQUESTS_STATUS,
+                              help_text=REQUESTS_STATUS_TEXT, default='drf')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
-        return '{0} /{1} /{2}'.format(self.user, self.creation_date, self.status)
+        return '{0} /{1} /{2}'.format(self.user, self.created_at, self.status)
 
     class Meta:
-        ordering = ['creation_date']
+        ordering = ['created_at']
         verbose_name = 'User request'
         verbose_name_plural = 'User requests'
 
 
 class ExtendingUser(models.Model):
     user = models.OneToOneField(django_settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    is_user = models.BooleanField(help_text='Select to assign user as ordinary user', default=True)
-    is_operator = models.BooleanField(help_text='Select to assign user as operator', default=False)
-    is_admin = models.BooleanField(help_text='Select to assign user as admin', default=False)
+    roles = models.ManyToManyField('UserRoles', through='AssignedRoles')
 
     def __str__(self):
         return 'Extending info for user {}'.format(self.user.username)
+
+    def check_group(self, group):
+        user_roles = list(AssignedRoles.objects.filter(
+            user_id=self.user.id).values_list('role__role_name', flat=True))
+        return group in user_roles
 
     class Meta:
         ordering = ['user']
         verbose_name = 'Extending User info'
         verbose_name_plural = 'Extending User info'
+
+
+class UserRoles(models.Model):
+    role_name = models.CharField(max_length=20, choices=constants.USER_ROLES,
+                                 help_text=USER_ROLES_TEXT,
+                                 default='usr')
+
+    def __str__(self):
+        return self.role_name
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'User role'
+        verbose_name_plural = 'User roles'
+
+
+class AssignedRoles(models.Model):
+    user = models.ForeignKey(ExtendingUser, on_delete=models.CASCADE)
+    role = models.ForeignKey(UserRoles, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "custom_requests_assignedroles"
