97b4b45e07f9b9d9c9f7e07c1cc9490766a0e01f
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-25 23:28:12 +0300

    Adds serializers for endpoint requests for user roles

--- custom_requests/serializers.py
+++ custom_requests/serializers.py
@@ -3,14 +3,41 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from . import models
-from .models import ExtendingUser
 
 
-class RequestsSerializer(serializers.ModelSerializer):
+class RequestsForUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Requests
         fields = '__all__'
+        # read_only_fields = ['user', 'status', 'created_at']
+
+    def validate_status(self, value):
+        if self.instance:
+            raise serializers.ValidationError("The user cannot change the status of his request")
+        return value
+
+    def validate_user(self, value):
+        raise serializers.ValidationError("You cannot change the author of the request")
+
+
+class RequestsForOperatorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Requests
+        fields = '__all__'
+        # read_only_fields = ['user', 'text', 'created_at']
+
+    def validate_text(self, value):
+        raise serializers.ValidationError("The operator cannot change the text of user requests")
+
+    def validate_status(self, value):
+        if value == 'drf':
+            raise serializers.ValidationError("The operator can only accept or reject user requests")
+        return value
+
+    def validate_user(self, value):
+        raise serializers.ValidationError("You cannot change the author of the request")
 
 
 class UsersSerializer(serializers.ModelSerializer):
@@ -18,17 +45,3 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        extending_user = ExtendingUser.objects.get(user=instance.id)
-        for key in ExtendingUserSerializer(extending_user).data:
-            representation[key] = ExtendingUserSerializer(extending_user).data[key]
-        return representation
-
-
-class ExtendingUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = ExtendingUser
-        fields = ['is_user', 'is_operator', 'is_admin']
