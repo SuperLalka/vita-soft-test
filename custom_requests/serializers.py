ad2569b80354fc52258afe6a211538fd10fca1ec
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-27 01:04:04 +0300

    Optimizes the logic of views & serializers

--- custom_requests/serializers.py
+++ custom_requests/serializers.py
@@ -5,43 +5,62 @@ from rest_framework import serializers
 from . import models
 
 
-class RequestsForUserSerializer(serializers.ModelSerializer):
-
+class RequestsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Requests
         fields = '__all__'
         # read_only_fields = ['user', 'status', 'created_at']
 
     def validate_status(self, value):
-        if self.instance:
-            raise serializers.ValidationError("The user cannot change the status of his request")
+        if self.context['request'].user.extendinguser.check_group('usr') and \
+                self.instance and value not in ['drf', 'snt']:
+            raise serializers.ValidationError(
+                "The user can change the status of his request only to 'sent'")
+        elif self.context['request'].user.extendinguser.check_group('opr') and \
+                value not in ['acc', 'rej']:
+            raise serializers.ValidationError(
+                "The operator can change the status of requests only to 'accepted' and 'rejected'")
+        return value
+
+    def validate_text(self, value):
+        if self.context['request'].user.extendinguser.check_group('opr'):
+            raise serializers.ValidationError(
+                "The operator cannot change the text of user requests")
         return value
 
     def validate_user(self, value):
-        raise serializers.ValidationError("You cannot change the author of the request")
+        raise serializers.ValidationError(
+            "You cannot change the author of the request")
 
 
-class RequestsForOperatorSerializer(serializers.ModelSerializer):
-
+class RequestsForUserAndOperatorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Requests
         fields = '__all__'
-        # read_only_fields = ['user', 'text', 'created_at']
-
-    def validate_text(self, value):
-        raise serializers.ValidationError("The operator cannot change the text of user requests")
-
-    def validate_status(self, value):
-        if value == 'drf':
-            raise serializers.ValidationError("The operator can only accept or reject user requests")
-        return value
+        # read_only_fields = ['user', 'status', 'created_at']
 
     def validate_user(self, value):
-        raise serializers.ValidationError("You cannot change the author of the request")
+        raise serializers.ValidationError(
+            "You cannot change the author of the request")
 
 
 class UsersSerializer(serializers.ModelSerializer):
+    role_name = serializers.CharField(max_length=3, required=False, write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'role_name']
+
+    def validate_role_name(self, value):
+        if value != 'opr':
+            raise serializers.ValidationError(
+                "The administrator can only designate a user as an 'operator'")
+        return self.instance.extendinguser.roles.all()
+
+    def update(self, instance, validated_data):
+        operator_role, _ = models.UserRoles.objects.get_or_create(role_name='opr')
+        for x in validated_data['role_name']:
+            if x.role_name == 'usr':
+                instance.extendinguser.roles.clear()
+        instance.extendinguser.roles.add(operator_role)
+        return instance
--- custom_requests/views.py
+++ custom_requests/views.py
@@ -1,19 +1,22 @@
-from django.contrib.auth.models import User
+from django.db.models import Q
 
 from rest_framework import viewsets, mixins
+from rest_framework import permissions as drf_permissions
 from rest_framework.viewsets import GenericViewSet
 
 from . import models, permissions, serializers
 
 
 class RequestsViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.RequestsSerializer
     lookup_field = 'id'
+    permission_classes = [drf_permissions.IsAuthenticated]
     permission_classes_by_action = {'create': [permissions.UserPermission],
                                     'retrieve': [permissions.OperatorPermission | permissions.UserPermission],
                                     'list': [permissions.OperatorPermission | permissions.UserPermission],
-                                    'update': [permissions.UserPermission],
-                                    'partial_update': [permissions.UserPermission],
-                                    'destroy': []}
+                                    'update': [permissions.OperatorPermission | permissions.UserPermission],
+                                    'partial_update': [permissions.OperatorPermission | permissions.UserPermission],
+                                    'destroy': [drf_permissions.IsAdminUser]}
 
     def get_permissions(self):
         try:
@@ -22,16 +25,19 @@ class RequestsViewSet(viewsets.ModelViewSet):
             return [permission() for permission in self.permission_classes]
 
     def get_queryset(self):
-        if self.request.user.extendinguser.check_group('usr'):
+        if self.request.user.extendinguser.check_group('usr') and \
+                self.request.user.extendinguser.check_group('opr'):
+            return models.Requests.objects.filter(Q(user=self.request.user, status='drf') and Q(status='snt'))
+        elif self.request.user.extendinguser.check_group('usr'):
             return models.Requests.objects.filter(user=self.request.user, status='drf')
         elif self.request.user.extendinguser.check_group('opr'):
             return models.Requests.objects.filter(status='snt')
 
     def get_serializer_class(self):
-        if self.request.user.extendinguser.check_group('usr'):
-            return serializers.RequestsForUserSerializer
-        elif self.request.user.extendinguser.check_group('opr'):
-            return serializers.RequestsForOperatorSerializer
+        if self.request.user.extendinguser.check_group('usr') and \
+                self.request.user.extendinguser.check_group('opr'):
+            return serializers.RequestsForUserAndOperatorSerializer
+        return self.serializer_class
 
 
 class UsersViewSet(mixins.ListModelMixin,
@@ -42,6 +48,7 @@ class UsersViewSet(mixins.ListModelMixin,
     permission_classes = [permissions.AdminPermission]
 
     def get_queryset(self):
-        users_list = list(models.AssignedRoles.objects.filter(
-            role__role_name='usr').values_list('user__user_id', flat=True))
+        role, _ = models.UserRoles.objects.get_or_create(role_name='usr')
+        users_list = list(models.ExtendingUser.objects.filter(roles=role).
+                          values_list('user__id', flat=True))
         return models.User.objects.filter(id__in=users_list)
